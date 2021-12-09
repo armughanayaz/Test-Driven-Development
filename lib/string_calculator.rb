3b3aaaa70e6c1d5b14556f4bbd38d5e3e89b1c1d
armughanayaz <armughanayaz@hotmail.com>
2021-12-08 21:23:05 -0500

    week 10 initail commit

---
+++ lib/string_calculator.rb
@@ -0,0 +1,13 @@
+class StringCalculator
+
+     def self.add(input)
+          string_parsed = input.tr('^0-9', '')
+          string_parsed_size = string_parsed.size
+          
+          if string_parsed.size == 0
+              string_parsed = nil
+              string_parsed_size = 0
+          end
+          {size: string_parsed_size, value: string_parsed }
+     end
+ end
